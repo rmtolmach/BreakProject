8e212dad630abd211c0947bfd330dbd12b7b9db3
Kelly <kelly_devlin@alumni.brown.edu>
2015-11-22 17:26:00 -0800

    Create edit and new forms with corresponding controller actions

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -7,4 +7,29 @@ class NeighborhoodsController < ApplicationController
     @neighborhood = Neighborhood.find(params[:id])
     @activities = @neighborhood.activities
   end
+
+  def edit
+    @neighborhood = Neighborhood.find(params[:id])
+  end
+
+  def update
+    neighborhood = Neighborhood.find(params[:id])
+    neighborhood.update(neighborhood_params)
+    redirect_to neighborhoods_path
+  end
+
+  def new
+    @neighborhood = Neighborhood.new
+  end
+
+  def create
+    @neighborhood = Neighborhood.create(neighborhood_params)
+    redirect_to neighborhoods_path
+  end
+
+  private
+
+  def neighborhood_params
+    params.require(:neighborhood).permit([:name, :description])
+  end
 end
