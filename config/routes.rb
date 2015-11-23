a88a53360819a52d41ad3ca089cbd2b8b41ab846
Kelly <kelly_devlin@alumni.brown.edu>
2015-11-22 16:07:44 -0800

    Create welcome index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  root 'welcome#index'
 
   resources :neighborhoods do
     resources :activities
   end
+
+  resources :activities
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
