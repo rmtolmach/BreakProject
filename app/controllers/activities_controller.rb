0df681501096aa17c32ca2cfaaa5409588fa14e2
Kelly <kelly_devlin@alumni.brown.edu>
2015-11-24 13:21:06 -0800

    Create new and edit activity form, move when array to activities_helper

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,62 +1,38 @@
 class ActivitiesController < ApplicationController
   def show
     @activity = Activity.find(params[:id])
-    @when = []
-    @when.push("Monday Mornings")if @activity.monday_morning
-    @when.push("Monday Afternoons") if @activity.monday_afternoon
-    @when.push("Monday Evenings") if @activity.monday_evening
-    @when.push("Tuesday Mornings") if @activity.tuesday_morning
-    @when.push("Tuesday Afternoons") if @activity.tuesday_afternoon
-    @when.push("Tuesday Evenings") if @activity.tuesday_evening
-    @when.push("Wednesday Mornings") if @activity.wednesday_morning
-    @when.push("Wednesday Afternoons") if @activity.wednesday_afternoon
-    @when.push("Wednesday Evenings") if @activity.wednesday_evening
-    @when.push("Thursday Mornings") if @activity.thursday_morning
-    @when.push("Thursday Afternoons") if @activity.thursday_afternoon
-    @when.push("Thursday Evenings") if @activity.thursday_evening
-    @when.push("Friday Mornings") if @activity.friday_morning
-    @when.push("Friday Afternoons") if @activity.friday_afternoon
-    @when.push("Friday Evenings") if @activity.friday_evening
-    @when.push("Saturday Mornings") if @activity.saturday_morning
-    @when.push("Saturday Afternoons") if @activity.saturday_afternoon
-    @when.push("Saturday Evenings") if @activity.saturday_evening
-    @when.push("Sunday Mornings") if @activity.sunday_morning
-    @when.push("Sunday Afternoons") if @activity.sunday_afternoon
-    @when.push("Sunday Afternoons") if @activity.sunday_evening
-    case @activity.price_range
-    when "0"
-      @price_range = "Free"
-    when "1-5"
-      @price_range = "$1 - $5"
-    when "5-10"
-      @price_range = "$5 - $10"
-    when "10-5"
-      @price_range = "$10 - $15"
-    when "15-20"
-      @price_range = "$15 - $20"
-    else
-      @price_range = "Over $20"
-    end
   end
 
   def edit
     @activity = Activity.find(params[:id])
+    @categories = Activity.pluck(:category).uniq
+    @subcategories = Activity.pluck(:subcategory).uniq
+    @price_range = Activity.pluck(:price_range).uniq
+    @method = Activity.pluck(:method).uniq
+    @neighborhood = Neighborhood.find(params[:neighborhood_id])
   end
 
   def update
     activity = Activity.find(params[:id])
     activity.update(activity_params)
-    redirect_to neighborhood_path
+    redirect_to activity.neighborhood
   end
 
   def new
     @activity = Activity.new
+    @categories = Activity.pluck(:category).uniq
+    @subcategories = Activity.pluck(:subcategory).uniq
+    @price_range = Activity.pluck(:price_range).uniq
+    @method = Activity.pluck(:method).uniq
+    @neighborhood = Neighborhood.find(params[:neighborhood_id])
   end
 
   def create
     #need to add in neighborhood_id
-    @activity = Activity.create(activity_params)
-    redirect_to neighborhood_path
+    activity = Activity.create(activity_params) do |a|
+      a.neighborhood = Neighborhood.find(params[:neighborhood_id])
+    end
+    redirect_to activity.neighborhood
   end
 
   def destroy
--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,2 +1,83 @@
 module ActivitiesHelper
+  def times_when(activity)
+    times_when = []
+    times_when.push("Monday Mornings")if activity.monday_morning
+    times_when.push("Monday Afternoons") if activity.monday_afternoon
+    times_when.push("Monday Evenings") if activity.monday_evening
+    times_when.push("Tuesday Mornings") if activity.tuesday_morning
+    times_when.push("Tuesday Afternoons") if activity.tuesday_afternoon
+    times_when.push("Tuesday Evenings") if activity.tuesday_evening
+    times_when.push("Wednesday Mornings") if activity.wednesday_morning
+    times_when.push("Wednesday Afternoons") if activity.wednesday_afternoon
+    times_when.push("Wednesday Evenings") if activity.wednesday_evening
+    times_when.push("Thursday Mornings") if activity.thursday_morning
+    times_when.push("Thursday Afternoons") if activity.thursday_afternoon
+    times_when.push("Thursday Evenings") if activity.thursday_evening
+    times_when.push("Friday Mornings") if activity.friday_morning
+    times_when.push("Friday Afternoons") if activity.friday_afternoon
+    times_when.push("Friday Evenings") if activity.friday_evening
+    times_when.push("Saturday Mornings") if activity.saturday_morning
+    times_when.push("Saturday Afternoons") if activity.saturday_afternoon
+    times_when.push("Saturday Evenings") if activity.saturday_evening
+    times_when.push("Sunday Mornings") if activity.sunday_morning
+    times_when.push("Sunday Afternoons") if activity.sunday_afternoon
+    times_when.push("Sunday Afternoons") if activity.sunday_evening
+    times_when
+  end
+
+  def us_states
+    [
+      ['AK', 'AK'],
+      ['AL', 'AL'],
+      ['AR', 'AR'],
+      ['AZ', 'AZ'],
+      ['CA', 'CA'],
+      ['CO', 'CO'],
+      ['CT', 'CT'],
+      ['DC', 'DC'],
+      ['DE', 'DE'],
+      ['FL', 'FL'],
+      ['GA', 'GA'],
+      ['HI', 'HI'],
+      ['IA', 'IA'],
+      ['ID', 'ID'],
+      ['IL', 'IL'],
+      ['IN', 'IN'],
+      ['KS', 'KS'],
+      ['KY', 'KY'],
+      ['LA', 'LA'],
+      ['MA', 'MA'],
+      ['MD', 'MD'],
+      ['ME', 'ME'],
+      ['MI', 'MI'],
+      ['MN', 'MN'],
+      ['MO', 'MO'],
+      ['MS', 'MS'],
+      ['MT', 'MT'],
+      ['NC', 'NC'],
+      ['ND', 'ND'],
+      ['NE', 'NE'],
+      ['NH', 'NH'],
+      ['NJ', 'NJ'],
+      ['NM', 'NM'],
+      ['NV', 'NV'],
+      ['NY', 'NY'],
+      ['OH', 'OH'],
+      ['OK', 'OK'],
+      ['OR', 'OR'],
+      ['PA', 'PA'],
+      ['RI', 'RI'],
+      ['SC', 'SC'],
+      ['SD', 'SD'],
+      ['TN', 'TN'],
+      ['TX', 'TX'],
+      ['UT', 'UT'],
+      ['VA', 'VA'],
+      ['VT', 'VT'],
+      ['WA', 'WA'],
+      ['WI', 'WI'],
+      ['WV', 'WV'],
+      ['WY', 'WY']
+    ]
+  end
 end
