705398d27aa504d9809a25e56d9cf32dfcc1b6c3
Dreedle <davis.audrey.lynn@gmail.com>
2015-12-29 09:07:49 -0800

    Flow control is working now for assigning classes to body depending on route

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,15 @@
 module ApplicationHelper
   def body_class
-    if request.path.include?('neighborhoods') && params[:id].nil?
+    if request.path == "/"
       "all-hoods"
-    elsif request.path == "/"
+    elsif request.path.include?('activities') && request.path.include?('neighborhoods')
+      "one-act"
+    elsif request.path.include?('neighborhoods') && params[:id].nil?
       "all-hoods"
     elsif request.path.include?('neighborhoods') && !params[:id].nil?
       "one-hood"
-    elsif request.path.include?('activities') && params[:id].nil?
+    else request.path.include?('activities') && params[:id].nil?
       "all-acts"
-    else request.path.include?('activities') && !params[:id].nil?
-      "one-act"
     end
   end
 end
