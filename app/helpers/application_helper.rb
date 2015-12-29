6dd14f9c0999c0f6e007aef89c45d7fe0937ca07
Dreedle <davis.audrey.lynn@gmail.com>
2015-12-29 08:21:54 -0800

    Adding a helper method to change body class so can change background image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def body_class
+    if request.path.include?('neighborhoods') && params[:id].nil?
+      "all-hoods"
+    elsif request.path == "/"
+      "all-hoods"
+    elsif request.path.include?('neighborhoods') && !params[:id].nil?
+      "one-hood"
+    elsif request.path.include?('activities') && params[:id].nil?
+      "all-acts"
+    else request.path.include?('activities') && !params[:id].nil?
+      "one-act"
+    end
+  end
 end
